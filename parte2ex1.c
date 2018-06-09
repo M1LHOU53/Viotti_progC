8e81cd77661d6e271e25be5bd9093daaec1066de
cl4rk <castdouglas@gmail.com>
2018-06-08 22:19:01 -0300

    Create parte2ex1.c

---
+++ parte2ex1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+float n1, n2, result;
+
+int main(){
+
+printf("Digite o primeiro numero\n");
+scanf("%f", &n1);
+
+while ( n2 == 0)
+{
+	
+
+	printf("Digite o segundo numero\n");
+	scanf("%f", &n2);
+	
+
+	
+	
+}
+
+result = n1 /n2;
+
+
+
+printf("O resultado da divisão entre %.2f e %.2f e igual a %.3f: \n",n1, n2, result );
+
+}
