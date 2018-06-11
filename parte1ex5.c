f526dad7007d4285ad2b234fc9c0dc38d21f02aa
cl4rk <castdouglas@gmail.com>
2018-06-11 17:51:53 -0300

    Update parte1ex5.c

--- parte1ex5.c
+++ parte1ex5.c
@@ -8,18 +8,18 @@ float altura, base, perm, diag, area;
 
 int main()
 {
-	printf("Digite a base do retangulo: \n");
-	scanf("%f", &base);
-	printf("Digite a altura do retangulo: \n");
-	scanf("%f", &altura);
+printf("Digite a base do retangulo: \n");
+scanf("%f", &base);
+printf("Digite a altura do retangulo: \n");
+scanf("%f", &altura);
 
-    perm = 2*(base + altura); 
-    area = base * altura;
-	diag = sqrt(pow(base,2) + pow(altura,2)) ;
+perm = 2*(base + altura); 
+area = base * altura;
+diag = sqrt(pow(base,2) + pow(altura,2)) ;
 
-	printf("O perimetro do retangulo e: %f \n", perm );
-	printf("A area do retangulo e: %f M² \n" ,area );
-	printf("A diagonal do retangulo e: %f \n", diag );
+printf("O perimetro do retangulo e: %f \n", perm );
+printf("A area do retangulo e: %f M² \n" ,area );
+printf("A diagonal do retangulo e: %f \n", diag );
 
 
 }
